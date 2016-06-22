c2c9714b63a996ac1dc12de0a19cfb0e6028d375
Raul7 <mrzero7@163.com>
2016-06-22 14:31:07 +0800

    Initial Commit

---
+++ XHPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHPlayer
+//
+//  Created by TianGeng on 16/6/22.
+//  Copyright © 2016年 bykernel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XHPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XHPlayer
+//
+//  Created by TianGeng on 16/6/22.
+//  Copyright © 2016年 bykernel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
